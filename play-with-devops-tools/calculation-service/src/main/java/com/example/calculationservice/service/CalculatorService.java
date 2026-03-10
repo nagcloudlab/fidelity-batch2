44df7ef198c6a9e573ac186dcc55c45824ec5094
nagcloudlab <67089749+nagcloudlab@users.noreply.github.com>
2026-03-10 10:41:21 +0530

    fix

--- play-with-devops-tools/calculation-service/src/main/java/com/example/calculationservice/service/CalculatorService.java
+++ play-with-devops-tools/calculation-service/src/main/java/com/example/calculationservice/service/CalculatorService.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Service;
 public class CalculatorService {
 
     public int add(int a, int b) {
-        return a + b;
+        return a - b;
     }
 
     public int subtract(int a, int b) {
